3440153fca06d1b3476585995060b823917ee18b
ssamarin <semenssamarin@mail.com>
2022-06-28 19:35:02 +0900

    phone mask

--- src/js/script.js
+++ src/js/script.js
@@ -78,4 +78,5 @@ $(document).ready(function(){
     validateForms('#consultation-form');
     validateForms('#consultation form');
     validateForms('#order form');
+    $('input[name=phone]').mask("+7 (999) 999-99-99")
 });
