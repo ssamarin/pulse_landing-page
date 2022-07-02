15547f0a16b60cda513957f13d35e0101eb02454
ssamarin <semenssamarin@mail.com>
2022-07-02 18:30:48 +0900

    a lot of

--- src/js/script.js
+++ src/js/script.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
-    // MODAL
+    // Modal
 
     $('[data-modal=consultation]').on('click', function() {
         $('.overlay, #consultation').fadeIn('slow');
@@ -47,8 +47,9 @@ $(document).ready(function(){
         $(this).on('click', function() {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
-        });
+        })
     });
+
     function validateForms(form){
         $(form).validate({
             rules: {
@@ -64,19 +65,54 @@ $(document).ready(function(){
             },
             messages: {
                 name: {
-                    required: "Пиши имя",
-                    minlength: jQuery.validator.format("Давай хотя бы {0} символа!")
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
                   },
-                phone: "Пиши телефон",
+                phone: "Пожалуйста, введите свой номер телефона",
                 email: {
-                  required: "Пиши почту",
-                  email: "Нормальную напиши"
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
                 }
             }
         });
     };
+
     validateForms('#consultation-form');
     validateForms('#consultation form');
     validateForms('#order form');
-    $('input[name=phone]').mask("+7 (999) 999-99-99")
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
